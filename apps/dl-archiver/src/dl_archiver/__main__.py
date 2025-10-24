b2b93ef606529298d488535f2f1e837b1f9f5821
David Christie <david.christie@mediaradar.com>
2025-10-23 23:27:18 -0400

    Add workspace members and ruff

---
+++ apps/dl-archiver/src/dl_archiver/__main__.py
@@ -0,0 +1,7 @@
+# Make the package executable/runnable by adding this file
+# you can run this package by running a command similar to python3 -m dl_archiver
+
+from dl_archiver import main
+
+if __name__ == "__main__":
+    main()
---
+++ apps/dl-efs-cleanup/src/dl_efs_cleanup/__main__.py
@@ -0,0 +1,7 @@
+# Make the package executable/runnable by adding this file
+# you can run this package by running a command similar to python3 -m dl_efs_cleanup
+
+from dl_efs_cleanup import main
+
+if __name__ == "__main__":
+    main()
